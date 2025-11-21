e306eeb62b0b637c5830f1b9feb031f7570ea184
Wenle Z <wenle.zeng@mail.utoronto.ca>
2025-11-21 18:00:30 -0500

    Add required helper methods for MarkUs tasks

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -36,32 +36,7 @@ public class StatementPrinter {
         for (Performance p : invoice.getPerformances()) {
 
             final Play play = plays.get(p.getPlayID());
-            int thisAmount = 0;
-
-            switch (play.getType()) {
-                case "tragedy":
-                    thisAmount = Constants.TRAGEDY_BASE_AMOUNT;
-                    if (p.getAudience() > Constants.TRAGEDY_AUDIENCE_THRESHOLD) {
-                        thisAmount += Constants.TRAGEDY_OVER_BASE_CAPACITY_PER_PERSON
-                                * (p.getAudience() - Constants.TRAGEDY_AUDIENCE_THRESHOLD);
-                    }
-                    break;
-
-                case "comedy":
-                    thisAmount = Constants.COMEDY_BASE_AMOUNT;
-
-                    if (p.getAudience() > Constants.COMEDY_AUDIENCE_THRESHOLD) {
-                        thisAmount += Constants.COMEDY_OVER_BASE_CAPACITY_AMOUNT
-                                + Constants.COMEDY_OVER_BASE_CAPACITY_PER_PERSON
-                                * (p.getAudience() - Constants.COMEDY_AUDIENCE_THRESHOLD);
-                    }
-
-                    thisAmount += Constants.COMEDY_AMOUNT_PER_AUDIENCE * p.getAudience();
-                    break;
-
-                default:
-                    throw new RuntimeException(String.format("unknown type: %s", play.getType()));
-            }
+            int thisAmount = calculateAmount(play, p);
 
             // volume credits
             volumeCredits += Math.max(
@@ -94,5 +69,87 @@ public class StatementPrinter {
         ));
 
         return result.toString();
+    }
+
+    // ------------------------------
+    // ⭐ REQUIRED METHODS FOR MARKUS
+    // ------------------------------
+
+    /** MarkUs requires this helper */
+    public int getAmount(Play play, Performance perf) {
+        return calculateAmount(play, perf);
+    }
+
+    /** MarkUs requires this helper */
+    public Play getPlay(String playID) {
+        return plays.get(playID);
+    }
+
+    /** MarkUs requires this helper */
+    public int getVolumeCredits(Play play, Performance perf) {
+        int volumeCredits = Math.max(
+                perf.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD,
+                0
+        );
+        if ("comedy".equals(play.getType())) {
+            volumeCredits += perf.getAudience() / Constants.COMEDY_EXTRA_VOLUME_FACTOR;
+        }
+        return volumeCredits;
+    }
+
+    /** MarkUs requires this helper */
+    public String usd(int amount) {
+        return NumberFormat.getCurrencyInstance(Locale.US)
+                .format((double) amount / Constants.PERCENT_FACTOR);
+    }
+
+    /** MarkUs requires this helper */
+    public int getTotalAmount() {
+        int total = 0;
+        for (Performance perf : invoice.getPerformances()) {
+            Play play = plays.get(perf.getPlayID());
+            total += calculateAmount(play, perf);
+        }
+        return total;
+    }
+
+    /** MarkUs requires this helper */
+    public int getTotalVolumeCredits() {
+        int credits = 0;
+        for (Performance perf : invoice.getPerformances()) {
+            Play play = plays.get(perf.getPlayID());
+            credits += getVolumeCredits(play, perf);
+        }
+        return credits;
+    }
+
+    // ------------------------------
+    // INTERNAL SHARED LOGIC
+    // ------------------------------
+    private int calculateAmount(Play play, Performance perf) {
+        int amount = 0;
+        switch (play.getType()) {
+            case "tragedy":
+                amount = Constants.TRAGEDY_BASE_AMOUNT;
+                if (perf.getAudience() > Constants.TRAGEDY_AUDIENCE_THRESHOLD) {
+                    amount += Constants.TRAGEDY_OVER_BASE_CAPACITY_PER_PERSON
+                            * (perf.getAudience() - Constants.TRAGEDY_AUDIENCE_THRESHOLD);
+                }
+                break;
+
+            case "comedy":
+                amount = Constants.COMEDY_BASE_AMOUNT;
+                if (perf.getAudience() > Constants.COMEDY_AUDIENCE_THRESHOLD) {
+                    amount += Constants.COMEDY_OVER_BASE_CAPACITY_AMOUNT
+                            + Constants.COMEDY_OVER_BASE_CAPACITY_PER_PERSON
+                            * (perf.getAudience() - Constants.COMEDY_AUDIENCE_THRESHOLD);
+                }
+                amount += Constants.COMEDY_AMOUNT_PER_AUDIENCE * perf.getAudience();
+                break;
+
+            default:
+                throw new RuntimeException("unknown type: " + play.getType());
+        }
+        return amount;
     }
 }
